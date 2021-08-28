77376ddfb080da731a7f06aa4dea75974e1748e9
Aroy-Art <39088602+Aroy-Art@users.noreply.github.com>
2021-08-28 11:29:42 +0200

    Fixed move command

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ def backup_old_user_configs():
         for f in user_backup_files:
             if os.path.exists(home_folder + "/" + f):
                 print("Exists (Backing up)   :", home_folder + "/" + f)
-                os.system("mv -v -i " + home_folder + "/" + f + " " + home_folder + "/Backup")
+                os.system("mv -v -i " + home_folder + "/" + f + " " + home_folder + "/Backup/")
             else:
                 print("Doesn't exists skiping:", home_folder + "/" + f)
 
